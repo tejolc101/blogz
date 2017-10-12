82e1ed0310731d4165504b1ce58194d8b79ea5a1
unknown <teja.uppu@gmail.com>
2017-10-11 21:55:05 -0700

    finished

--- main.py
+++ main.py
@@ -24,16 +24,18 @@ class Blog(db.Model):
 @app.route('/main-blog', methods=['POST','GET'])
 def index():
     
-    title_name = request.args.get('title')
-    body_name = request.args.get('body')
+    #title_name = request.args.get('title')
+    #body_name = request.args.get('body')
     
-    id = request.args.get('id')
+    blog_id = request.args.get('id')
     #id = request.form['id']
     
     
     blogs = Blog.query.all()
-    if len(id):
-        return render_template("new-post.html",title_name =title_name, body=body_name)
+    if blog_id:
+        
+        blog = Blog.query.get(blog_id)
+        return render_template("new-post.html",blog = blog)
     else:
         return render_template("blog-posts.html",blogs=blogs,title="Main-Blog")
     
@@ -47,25 +49,28 @@ def new_post():
 def new_blog():
     error_title =""
     error_body=""
+    
     if request.method == 'GET':
-        #id = request.args.get('id')
+        
         
         title_name = request.args.get('title')
         body_name = request.args.get('body')
         new_body = Blog(title_name, body_name)
         db.session.add(new_body)
-        db.session.commit() 
-    
+        db.session.commit()
+        new_title = Blog.query.filter_by(title=title_name).first() 
+        blog_id = new_title.id
+        
+
         if title_name == "":
             error_title ="Please fill in the title"
         if body_name == "":
             error_body = "Please fill int the content"
 
         if not error_title and not error_body:
-            #if id != None:
-            return redirect('/main-blog')
-                    
-            #return render_template("new-post.html",title_name =title_name, body=body_name)
+           
+            return redirect('/main-blog?id={0}'.format(blog_id))
+            #return render_template("new-post.html",blog=new_body)
         else:
             return render_template('todos.html',title="Build a Blog",error_title=error_title,error_body=error_body)
 
